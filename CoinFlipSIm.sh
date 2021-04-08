79bae1af245d0cc90dc4d66478b73a17d9a0454b
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-09 00:29:48 +0530

    Added : game continues till difference of minimum 2 points is achived

--- CoinFlipSIm.sh
+++ CoinFlipSIm.sh
@@ -5,29 +5,36 @@ tails=0;
 heads=0;
 
 i=0;
-while [ $tails -lt 21 -a $heads -lt 21 ]
+while [ true ]
 do 
 
 	TossCheck=$((RANDOM%2));
 	if [ $TossCheck -eq 0 ];
 	then
 		tails=$(($tails+1));
-		echo Tails Win;
+		echo Tails;
 	else
 		heads=$(($heads+1));
-		echo Heads Win;
+		echo Heads;
+	fi
+	
+	difft=$(($tails-$heads));
+	diffh=$(($heads-$tails));
+
+	if [ $tails -ge 21 -a $difft -ge 2 ]
+	then
+		echo "Tails won by: "$(($tails-$heads));
+
+		break;
+
+	elif [ $heads -ge 21 -a $diffh -ge 2 ]
+	then
+
+		echo "Heads won by: " $(($heads-$tails));
+
+		break;
 	fi
 
 done
 
 echo "Tails count: "$tails "and Heads count :"$heads;
-
-if [ $heads -gt $tails ]
-then
-	echo "Heads won by: "$(( $heads-$tails ));
-
-else
-	echo "Tails won by: "$(( $tails-$heads ));
-
-fi
-
