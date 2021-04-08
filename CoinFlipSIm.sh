4e897e6e4a81f3da7c29dc1d33570390040a8550
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-08 23:43:01 +0530

    Added : coin flip winner

--- CoinFlipSIm.sh
+++ CoinFlipSIm.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulation Program"
+
+tails=0;
+heads=0;
+TossCheck=$((RANDOM%2));
+if [ $TossCheck -eq 0 ];
+then
+	tails=$(($tails+1));
+	echo Tails Win;
+else
+	heads=$(($heads+1));
+	echo Heads Win;
+fi
+
+
